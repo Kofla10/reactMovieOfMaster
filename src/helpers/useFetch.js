fbddd668070f8387097a99c058909e855e55fffe
kofla10 <juank_milo94@hotmail.com>
2023-12-18 21:48:56 -0500

    my code do not work

--- src/helpers/useFetch.js
+++ src/helpers/useFetch.js
@@ -1,6 +1,7 @@
 export const useFetch = async (title) => {
 
     const API_URL = `http://www.omdbapi.com?apikey=c032e2d7&s=${title}`;
+    console.log(API_URL)
     const resp = await fetch(API_URL);
     const data = await resp.json();
 
